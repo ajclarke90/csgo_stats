1d17dd94091918be5f29863dccff3f8a8d4fb456
ajclarke <ajclarke90@gmail.com>
2014-11-18 23:34:40 +0000

    First commit

---
+++ __init__.py
@@ -0,0 +1,20 @@
+""" 
+
+csgo_project
+
+======
+
+This is a small project in working out how the damage of the guns in CSGO 
+scales with things like range / armor etc
+
+Written: Adam Clarke
+2014
+
+"""
+
+#Import the subtree stuff
+
+from . import gun_data
+from test import damage_calc_witharmor
+from test import damage_calc_noarmor
+from test import hitboxmodifier
