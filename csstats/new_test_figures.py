51c138626dbbce5b46e228736695c22b8c947438
ajclarke <ajclarke90@gmail.com>
2014-11-20 23:21:24 +0000

    toggling lines now works

--- csstats/new_test_figures.py
+++ csstats/new_test_figures.py
@@ -1,17 +1,20 @@
 # First set up the axis and figure enviroenmetn
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 fig, ax = plt.subplots()
 ax.set_title('Click on legend line to toggle line on/off')
 
+linedict = {}
 for weapon in weapons.weapons[:MAX_WEAPONS_TO_SHOW]:
     xpoints = range(0, CS_MAX_RANGE, 100)
     ypoints = [weapon.damagerange_calc_witharmor(distance, 'chest') for distance in xpoints]
-    linedict[ "{0}".format(weapon.name)] = ax.plot(xpoints,ypoints, label=weapon.name)
+    linedict[ "{0}".format(weapon.name)], = ax.plot(xpoints,ypoints, label=weapon.name)
 
 lines = linedict.values()
 leg = ax.legend(loc='upper right', fancybox=True, shadow=True)
 leg.get_frame().set_alpha(0.4)
-
 lined = dict()
 for legline, origline in zip(leg.get_lines(), lines):
     legline.set_picker(5)  # 5 pts tolerance
