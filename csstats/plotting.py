1008b2cfa207a6315bfa6ad63780e346c6773828
ajclarke <ajclarke90@gmail.com>
2014-11-25 22:06:17 +0000

    I forgot what i did whilst i tried working out what cl_max_draw_distance

--- csstats/plotting.py
+++ csstats/plotting.py
@@ -3,30 +3,32 @@ import matplotlib.pyplot as plt
 import weapons
 import pdb
 
+
+
 def plot_damage(wtype="pistol", firerange=2048, armor=True, location="head"):
-    
+
     if wtype not in {w.type for w in weapons.weapons}:
         raise ValueError("{0} not a valid weapon type".format(wtype))
     if location not in ["head", "chest", "stomach", "leg"]:
         raise ValueError("{0} not a valid hitbox location".format(location))
-    
     CS_MAX_RANGE = int(firerange) # 2048 is roughly the distance down DD2 long
     # First set up the axis and figure environment
     fig, ax = plt.subplots()
-    ax.set_title('Click on legend line to toggle line on/off')
+
     ax.set_xlabel('Range [units]')
     ax.set_ylabel('Damage [units]')
 
     linearray = []
     for weapon in [w for w in weapons.weapons if w.type == wtype]:
-        
-        xpoints = range(0, CS_MAX_RANGE, 100)
-        
+
+        xpoints = range(0, CS_MAX_RANGE, (CS_MAX_RANGE-0)/100)
+
         if armor:
             ypoints = [weapon.damagerange_calc_witharmor(distance, location) for distance in xpoints]
+            ax.set_title('Damage at Distance with Armor')
         else:
             ypoints = [weapon.damagerange_calc_noarmor(distance, location) for distance in xpoints]
-
+            ax.set_title('Damage at Distance without Armor')
         line, = ax.plot(xpoints,ypoints, label=weapon.name)
         linearray.append((weapon.name, line))
         lines = [a[1] for a in linearray]
@@ -54,6 +56,6 @@ def plot_damage(wtype="pistol", firerange=2048, armor=True, location="head"):
         fig.canvas.draw()
 
     fig.canvas.mpl_connect('pick_event', onpick)
-    plt.plot([0,CS_MAX_RANGE],[100,100], 'r-')
+    plt.plot([0,CS_MAX_RANGE],[100,100], 'r--')
 
     return plt
